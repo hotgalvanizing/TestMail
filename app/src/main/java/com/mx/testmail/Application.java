8f194bfcc6f2975fbc98252ee53d64b4127652d3
hotgalvanizing <steelhe@qq.com>
2019-05-28 07:28:11 +0800

    引入字体图标库

--- app/src/main/java/com/mx/testmail/Application.java
+++ app/src/main/java/com/mx/testmail/Application.java
@@ -1,14 +1,29 @@
 package com.mx.testmail;
 
+import com.joanzapata.iconify.fonts.FontAwesomeModule;
 import com.mx.corelibrary.app.CoreConfig;
+import com.mx.maillibrary.icon.FontEcModule;
 
 public class Application extends android.app.Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
         CoreConfig.init(this)
+                .withIcon(new FontAwesomeModule())
+                .withIcon(new FontEcModule())
+                .withLoaderDelayed(1000)
                 .withApiHost("http://mock.fulingjie.com/mock/api/")
+//                .withInterceptor(new DebugInterceptor("test", R.raw.test))
+//                .withWeChatAppId("你的微信AppKey")
+//                .withWeChatAppSecret("你的微信AppSecret")
+//                .withJavascriptInterface("latte")
+//                .withWebEvent("test", new TestEvent())
+//                .withWebEvent("share", new ShareEvent())
+                //添加Cookie同步拦截器
+                .withWebHost("https://www.baidu.com/")
+//                .withInterceptor(new AddCookieInterceptor())
                 .configure();
     }
 }
--- corelibrary/src/main/java/com/mx/corelibrary/app/Configurator.java
+++ corelibrary/src/main/java/com/mx/corelibrary/app/Configurator.java
@@ -2,6 +2,10 @@ package com.mx.corelibrary.app;
 
 import android.os.Handler;
 
+import com.joanzapata.iconify.IconFontDescriptor;
+import com.joanzapata.iconify.Iconify;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
@@ -9,8 +13,11 @@ import java.util.HashMap;
  */
 public class Configurator {
 
-    private final static HashMap<Object, Object> CONFIGS = new HashMap();
     private static final Handler HANDLER = new Handler();
+    private static final ArrayList<IconFontDescriptor> ICONS = new ArrayList<>();
+//    private static final ArrayList<Interceptor> INTERCEPTORS = new ArrayList<>();
+
+    private final static HashMap<Object, Object> CONFIGS = new HashMap();
 
     private Configurator() {
         CONFIGS.put(ConfigKeys.CONFIG_READY, false);
@@ -47,18 +54,18 @@ public class Configurator {
     }
 
     private void initIcons() {
-//        if (ICONS.size() > 0) {
-//            final Iconify.IconifyInitializer initializer = Iconify.with(ICONS.get(0));
-//            for (int i = 1; i < ICONS.size(); i++) {
-//                initializer.with(ICONS.get(i));
-//            }
-//        }
+        if (ICONS.size() > 0) {
+            final Iconify.IconifyInitializer initializer = Iconify.with(ICONS.get(0));
+            for (int i = 1; i < ICONS.size(); i++) {
+                initializer.with(ICONS.get(i));
+            }
+        }
     }
 
-//    public final Configurator withIcon(IconFontDescriptor descriptor) {
-//        ICONS.add(descriptor);
-//        return this;
-//    }
+    public final Configurator withIcon(IconFontDescriptor descriptor) {
+        ICONS.add(descriptor);
+        return this;
+    }
 
 //    public final Configurator withInterceptor(Interceptor interceptor) {
 //        INTERCEPTORS.add(interceptor);
